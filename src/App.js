9d4ad8de451046a41a8362efaa51ae52c6ca2637
danielNempeque <daniel.nempeque@gmail.com>
2019-11-09 08:36:17 -0500

    Cosas

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import React, { Component } from 'react';
 import Counter from './containers/Counter/Counter';
 import './App.css';
 
+import { createStore, combineReducers } from 'redux';
+
+import { Provider } from 'react-redux';
+import counterReducer from './store/reducers/counter';
+
+const rootReducer = combineReducers({
+  counterStore: counterReducer
+});
+
+const store = createStore(rootReducer);
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <Counter />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Counter />
+        </div>
+      </Provider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-import counterReducer from './store/reducers/counter';
-
-const rootReducer = combineReducers ({
-    counterStore: counterReducer
-});
-
-const store = createStore(rootReducer);
-
-ReactDOM.render(
-    <Provider store = {store}>
-        <App />
-    </Provider>,
-document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
